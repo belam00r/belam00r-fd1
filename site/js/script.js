18fe2f6be446fb9469da6aaf61d816d63ee9db32
Vladimir Kukarekin <unlock.onliner@gmail.com>
2020-05-05 18:24:25 +0300

    dz

---
+++ site/js/script.js
@@ -0,0 +1,17 @@
+// Acc
+$(document).on("click", ".brand .menu div", function() {
+    var numberIndex = $(this).index();
+
+    if (!$(this).is("active")) {
+        $(".brand .menu div").removeClass("active");
+        $(".brand ul li").removeClass("active");
+
+        $(this).addClass("active");
+        $(".brand ul").find("li:eq(" + numberIndex + ")").addClass("active");
+
+        var listItemHeight = $(".brand ul")
+            .find("li:eq(" + numberIndex + ")")
+            .innerHeight();
+        // $(".brand ul").height(listItemHeight + "px");
+    }
+});

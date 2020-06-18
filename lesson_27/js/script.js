b8534d1595a8f5cde33a48492261f642ea782172
Vladimir Kukarekin <unlock.onliner@gmail.com>
2020-06-18 21:07:40 +0300

    DZ2

--- lesson_27/js/script.js
+++ lesson_27/js/script.js
@@ -7,7 +7,6 @@ $("form").on("submit", form)
 
 function form(e) {
     e.preventDefault()
-        // console.log(e)
     var form = e.target;
     // var name = form[0].value
     // console.log(name)
@@ -21,17 +20,20 @@ function form(e) {
     $.post("http://109.235.69.203:8080/data/1009", { name, date, car, result })
         .done(function() { console.log("Все хорошо") })
         .fail(function() { console.log("Все плохо") })
-    $(".table").append(`<tr>
-                            <td class="name">${name}</td>
-                            <td class="date">${date}</td>
-                            <td class="car">${car}</td>
-                            <td class="result">${result}</td>
-                        </tr>`)
-
-
-
-
-
-
 
 }
+$(".result-table").on("click", function(e) {
+    console.log(e)
+    $(".table").css('display', 'table');
+})
+$.get("http://109.235.69.203:8080/data/1009", function(e) {
+    for (var i = 0; i < e.length; i++) {
+        $(".table").append(`<tr>
+                            <td class="name">${e[i].name}</td>
+                            <td class="date">${e[i].date}</td>
+                            <td class="car">${e[i].car}</td>
+                            <td class="result">${e[i].result}</td>
+                        </tr>`)
+    }
+
+});
